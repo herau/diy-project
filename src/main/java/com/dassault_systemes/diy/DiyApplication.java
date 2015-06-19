07eaef3ecc0c5af751b2cebef57bbaab30fbbcff
n27 <n27@3ds.com>
2015-06-19 17:40:03 +0200

    spring boot init bower init index and account page

---
+++ src/main/java/com/dassault_systemes/diy/DiyApplication.java
@@ -0,0 +1,12 @@
+package com.dassault_systemes.diy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiyApplication.class, args);
+    }
+}
---
+++ src/test/java/com/dassault_systemes/diy/DiyApplicationTests.java
@@ -0,0 +1,18 @@
+package com.dassault_systemes.diy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DiyApplication.class)
+@WebAppConfiguration
+public class DiyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
