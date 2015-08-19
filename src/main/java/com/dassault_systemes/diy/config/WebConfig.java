986ab9a3572667f54448f29ddf5ecb52cebe5d31
Aurélien Leboulanger <leboulanger.aurelien@gmail.com>
2015-08-19 14:24:24 +0200

    switch to Spring boot 1.3.0 m3 to be able to define Jackson serialization include properties

--- src/main/java/com/dassault_systemes/diy/config/WebConfig.java
+++ src/main/java/com/dassault_systemes/diy/config/WebConfig.java
@@ -1,9 +1,6 @@
 package com.dassault_systemes.diy.config;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -16,13 +13,4 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         registry.addViewController("/").setViewName("account.html");
         registry.addViewController("/account").setViewName("account.html");
     }
-
-    @Bean
-    public Jackson2ObjectMapperBuilder jacksonBuilder() {
-        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
-        //        builder.modules(new Hibernate4Module());
-        builder.serializationInclusion(JsonInclude.Include.NON_NULL);
-        return builder;
-    }
-
 }
