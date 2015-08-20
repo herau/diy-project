8b1fc5b82ed8ab73034eb08bce5c696051e9447f
Aurélien Leboulanger <leboulanger.aurelien@gmail.com>
2015-08-20 15:53:57 +0200

    fix JSON serialization of Java 8 LocalDateTime to Timestamp

---
+++ src/main/java/com/dassault_systemes/diy/config/JacksonConfig.java
@@ -0,0 +1,16 @@
+package com.dassault_systemes.diy.config;
+
+import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    Module jsr310Module() {
+        return new JavaTimeModule();
+    }
+
+}
