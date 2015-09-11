7a83cc37b81ee5624da0f9775fda971ff6792508
Aurélien Leboulanger <leboulanger.aurelien@gmail.com>
2015-09-11 14:34:39 +0200

    remove useless entities : Supplier & Contact  remove localization field for a tool

--- src/main/resources/sql/import-suppliers.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO suppliers (name, city, street, zip, fax, phone)
-VALUES ('MABEO INDUSTRIES', 'Trappes', 'Z.I. des Bruyeres - 6, avenue Jean Rostand', 78190, '0134829403', '0134829200');
-INSERT INTO suppliers (name, city, street, zip, fax, phone) VALUES
-  ('CEGELEC NON DESTRUCTIVE TESTING', 'Brétigny-sur-orge', 'Z.I du Bois des Bordes – Le Plessis Pate', 91229,
-   '0169886788', '0169886767');
-INSERT INTO suppliers (name, city, street, zip, fax, phone)
-VALUES ('REVERT SA', 'Versailles Cedex', '12, rue Carnot RP921', 78009, '0139530598', '0139201515');
