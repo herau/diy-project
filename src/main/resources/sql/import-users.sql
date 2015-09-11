bd641ac513e2b260392420ca00b49866a9f42d2e
Aurélien Leboulanger <leboulanger.aurelien@gmail.com>
2015-09-11 17:09:02 +0200

    add accessory (with data) and equipment entities #8

--- src/main/resources/sql/import-users.sql
+++ src/main/resources/sql/import-users.sql
@@ -4,3 +4,6 @@ INSERT INTO users (personal_number, password, firstname, lastname, email, state,
 -- Application user
 INSERT INTO users (personal_number, password, firstname, lastname, email, state, company, role) VALUES
   ('321', '$2a$10$e2wcKCBAr/KtVOSh5eGQfO2tQpKN3SM22sSxXAU5rlqqb0RPWOBJa', 'bar', 'foo', 'bar.foo@3ds.com', 0, 0, 'USER');
+-- Member user
+INSERT INTO users (personal_number, password, firstname, lastname, email, state, company, role) VALUES
+  ('0123', '$2a$10$tq86j.XU4jgX2zbWpeyCqukHKZ93ZEiFvepqYRzWEm9vDqj4na3vK', 'fooBar', 'barFoo', 'foo.foo@3ds.com', 0, 0, 'MEMBER');
