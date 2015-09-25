8c002aa7e8482d806648682d03d7ad2845245bc8
Aurélien Leboulanger <leboulanger.aurelien@gmail.com>
2015-09-25 11:15:47 +0200

    #13 create a test page for update the csv file (upload users) and the entryPoint of the controller

--- src/main/java/com/ds/ce/diy/web/controllers/UserController.java
+++ src/main/java/com/ds/ce/diy/web/controllers/UserController.java
@@ -3,7 +3,9 @@ package com.ds.ce.diy.web.controllers;
 import com.ds.ce.diy.domain.Company;
 import com.ds.ce.diy.domain.State;
 import com.ds.ce.diy.domain.User;
+import com.ds.ce.diy.domain.VerificationToken;
 import com.ds.ce.diy.dto.UserDTO;
+import com.ds.ce.diy.dto.UserPasswordDTO;
 import com.ds.ce.diy.repositories.UserRepository;
 import com.ds.ce.diy.service.TokenService;
 import com.ds.ce.diy.service.UserService;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletResponse;
@@ -23,8 +26,7 @@ import javax.validation.Valid;
 
 import java.util.List;
 
-import static org.springframework.http.HttpStatus.CREATED;
-import static org.springframework.http.HttpStatus.NO_CONTENT;
+import static org.springframework.http.HttpStatus.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.web.bind.annotation.RequestMethod.*;
 
@@ -78,6 +80,21 @@ public class UserController extends AbstractController {
         service.update(id, userDTO);
     }
 
+    @RequestMapping(value = "/token/{token}", method = PATCH)
+    void updateWithToken(@PathVariable String token, @Valid @RequestBody UserPasswordDTO userPasswordDTO,
+                         HttpServletResponse response) {
+        //TODO test
+        VerificationToken verifiedToken = tokenService.verifyToken(token);
+
+        if (verifiedToken == null) {
+            response.setStatus(NOT_FOUND.value());
+            return;
+        }
+
+        service.validUser(verifiedToken, userPasswordDTO.getPassword());
+
+    }
+
     @PreAuthorize("hasAuthority('ADMIN')")
     @RequestMapping(value = "{id}", method = DELETE)
     @ResponseStatus(NO_CONTENT)
@@ -95,11 +112,21 @@ public class UserController extends AbstractController {
     }
 
     //TODO Delete
-    @RequestMapping(value = "/import", method = POST)
+    @RequestMapping(value = "/test-import", method = POST)
     void test() {
         User user = new User("test", "test", "test", "test", "n27@3ds.com", Company.DS, State.INVALID);
         userRepository.save(user);
         tokenService.sendEmailRegistrationToken(user);
+    }
+
+    @RequestMapping(value = "/upload", method = POST)
+    void importUsers(@RequestParam("name") String name, @RequestParam("file") MultipartFile file) {
+        if (!file.isEmpty()) {
+            return;
+        }
+
+        //TODO @pfs31110 parse the csv file and use the userService to create or update users
+        // cf UserService
     }
 
     @PreAuthorize("hasAnyAuthority('ADMIN')")
