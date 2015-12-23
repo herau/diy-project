76e5074e857b4fc6f98d3d0fd507a05291af3832
herau <leboulanger.aurelien@gmail.com>
2015-12-23 16:23:36 +0100

    add method in mailService to send calendar invitation add ical4j and a dedicated service to create Icalendar

--- src/main/java/com/ds/ce/diy/service/MailService.java
+++ src/main/java/com/ds/ce/diy/service/MailService.java
@@ -9,4 +9,14 @@ public interface MailService {
 
     void sendMail(User user, String subject, String content);
 
+    /**
+     * send an invitation through the Icalendar format https://www.ietf.org/rfc/rfc2445.txt
+     * TODO have to be invitation (with validation) or just automatic added entry in their calendar
+     * TODO need participant plus optional ?
+     * @param user the mail receiver
+     * @param subject the mail subject
+     * @param calendar String representation of an ICalendar
+     */
+    void sendCalendar(User user, String subject, String calendar);
+
 }
