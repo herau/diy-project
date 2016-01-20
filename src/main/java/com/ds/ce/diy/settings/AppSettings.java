c80b546c4f5841bd1d269dc8cd196a928d56bcf0
herau <leboulanger.aurelien@gmail.com>
2016-01-20 15:33:41 +0100

    use lombok for Configuration Properties Spring class

--- src/main/java/com/ds/ce/diy/settings/AppSettings.java
+++ src/main/java/com/ds/ce/diy/settings/AppSettings.java
@@ -1,5 +1,7 @@
 package com.ds.ce.diy.settings;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
@@ -8,6 +10,8 @@ import javax.annotation.PostConstruct;
 
 @Component
 @ConfigurationProperties(prefix = "app")
+@Getter
+@Setter
 public class AppSettings {
 
     private Email email;
@@ -19,54 +23,19 @@ public class AppSettings {
         Assert.notNull(email.registration.getTokenExpiration());
     }
 
-    public Email getEmail() {
-        return email;
-    }
-
-    public void setEmail(Email email) {
-        this.email = email;
-    }
-
+    @Getter
+    @Setter
     public static class Email {
 
         private Registration registration;
 
-        public Registration getRegistration() {
-            return registration;
-        }
-
-        public void setRegistration(Registration registration) {
-            this.registration = registration;
-        }
-
+        @Getter
+        @Setter
         public static class Registration {
 
             private int tokenExpiration;
 
             private String subject;
-
-            /**
-             * Email Subject
-             * @return
-             */
-            public String getSubject() {
-                return subject;
-            }
-
-            public void setSubject(String subject) {
-                this.subject = subject;
-            }
-
-            /**
-             * @return Email registration Token Expiry Time in Minutes
-             */
-            public int getTokenExpiration() {
-                return tokenExpiration;
-            }
-
-            public void setTokenExpiration(int tokenExpiration) {
-                this.tokenExpiration = tokenExpiration;
-            }
         }
     }
 
