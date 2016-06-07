5c5f98583ade07bb2317cb303410b626ad6056b2
Florian Maffini <florian.maffini@3ds.com>
2016-06-07 17:17:01 +0200

    fixing unit tests

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -72,7 +72,7 @@ module.exports = function(config) {
      * available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
      */
     browsers: [
-      // 'Chrome',
+      //'Chrome',
       'PhantomJS'
     ],
 
--- src/main/webapp/tests/app.spec.ts
+++ src/main/webapp/tests/app.spec.ts
@@ -1,10 +1,13 @@
 // Import necessary wrappers for Jasmine
 import { provide } from '@angular/core';
 import { beforeEachProviders, describe, expect, inject, it } from '@angular/core/testing';
-import { ROUTER_PROVIDERS, LocationStrategy, PathLocationStrategy, APP_BASE_HREF } from '@angular/router';
+import { PathLocationStrategy, LocationStrategy, APP_BASE_HREF } from '@angular/common';
+import { ROUTER_PROVIDERS } from '@angular/router';
+import { HTTP_PROVIDERS } from '@angular/http';
 
 // Load the implementations that should be tested
 import { App } from '../app/components/app';
+import { UserService } from "../app/services/user";
 
 describe('App', () => {
 
@@ -14,8 +17,10 @@ describe('App', () => {
     // These providers are needed to be injected as dependencies for `Location` object, itself a dependency for the `App` component.
     // Normally, this is performed by `bootstrap.ts` file.
     ROUTER_PROVIDERS,
+    HTTP_PROVIDERS,
     provide(APP_BASE_HREF, {useValue: '/'}),
-    provide(LocationStrategy, {useClass: PathLocationStrategy})
+    provide(LocationStrategy, {useClass: PathLocationStrategy}),
+    provide(UserService, {useClass: UserService})
   ]);
 
   it('should have a title', inject([App], (app) => {
